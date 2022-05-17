51f5e63da6800ea75993059ffe0b1617db5b99a1
Sunny Moore <moore.sunny@gmail.com>
2022-05-17 11:35:40 -0700

    Add ship variable to initialize

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,11 @@
 require './lib/ship'
 
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate, :ship
 
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
   end
 
 end
