e9b4e5916c37687e26fe4154d814531c909b964f
Shirley DeCesari <shirley.decesari@gmail.com>
2022-05-17 08:36:56 -0700

    Add setup test for ship class

---
+++ spec/ship_spec.rb
@@ -0,0 +1,13 @@
+require './lib/ship'
+require 'pry'
+
+RSpec.describe Ship do
+  before :each do
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+  it "exists and has attributes" do
+    expect(@cruiser).to be_a Ship
+    expect(@cruiser.name).to eq "Cruiser"
+    expect(@cruiser.length).to eq 3
+  end
+end
