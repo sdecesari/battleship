f1767545ff950eff7a11602994db8d0435348fea
Sunny Moore <moore.sunny@gmail.com>
2022-05-17 11:22:34 -0700

    Add test for Cell initialize

---
+++ spec/cell_spec.rb
@@ -0,0 +1,13 @@
+require 'lib/ship'
+require 'lib/cell'
+
+describe Cell do
+  before: each do
+    @cell = Cell.new("B4")
+  end
+  it "initializes and has attributes" do
+    expect(@cell).to be_a Cell
+    expect(@cell.coordinate).to eq("B4")
+
+  end
+end
