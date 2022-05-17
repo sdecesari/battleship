f2606f29491cd7f441c21beb96de7c2201d143e5
Sunny Moore <moore.sunny@gmail.com>
2022-05-17 14:47:07 -0700

    Add tests for render no ship, miss, and ship

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -42,6 +42,15 @@ describe Cell do
     cell_1 = Cell.new("B4")
     cell_1.fire_upon
 
-    expect(@cell.render).to eq("M")
+    expect(cell_1.render).to eq("M")
   end
+  it "render with ship starts out as '.'" do
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2 = Cell.new("C3")
+    cell_2.place_ship(cruiser)
+
+    expect(cell_2.render).to eq(".")
+    expect(cell_2.render(true)).to eq("S")
+  end
+
 end
