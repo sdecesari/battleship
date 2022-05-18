9b6e00eb539464419a65b1a157d2df929aaeac09
Sunny Moore <moore.sunny@gmail.com>
2022-05-18 11:02:25 -0700

    Add test of  validation of coordinate to board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,4 +11,11 @@ RSpec.describe Board do
     expect(@board).to be_a Board
     expect(@board.cells).to be_a Hash
   end
+  it "can tell if a coordinate is on the board" do
+    expect(@board.valid_coordinate?("A1")).to eq(true)
+    expect(@board.valid_coordinate?("D4")).to eq(true)
+    expect(@board.valid_coordinate?("A5")).to eq(false)
+    expect(@board.valid_coordinate?("E1")).to eq(false)
+    expect(@board.valid_coordinate?("A22")).to eq(false)
+  end
 end
