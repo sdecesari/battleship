c9e5b267c74533f09b9cd0fa16178ba93460f9ee
Sunny Moore <moore.sunny@gmail.com>
2022-05-22 15:30:40 -0700

    Test random ship placement and coordinate selection

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,3 +1,4 @@
+require 'rspec'
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
@@ -14,10 +15,16 @@ describe Computer do
     expect(@computer).to be_a Computer
   end
   it "has ships" do
-    expect(@computer.cruiser).to be_a Ship
-    expect(@computer.submarine).to be_a Ship
+    expect(@computer.ships[0]).to be_a Ship
+    expect(@computer.ships[1]).to be_a Ship
   end
   it "has a board" do
     expect(@computer.board).to be_a Board
+  end
+  it "ship_placement returns true when ships are placed correctly" do
+    expect(@computer.ship_placement).to eq(true)
+  end
+  it "can select a coordinate to fire on" do
+    @computer.shots_fired
   end
 end
