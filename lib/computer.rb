0b8381f212cbdc748784cc8e60e8e1ff7954f136
Sunny Moore <moore.sunny@gmail.com>
2022-05-22 14:13:12 -0700

    Computer has ships

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,6 +1,10 @@
 class Computer
+  attr_reader :turns, :board, :cruiser, :submarine
 
   def initialize()
     @turns = []
+    @board = Board.new
+    @cruiser = Ship.new('cruiser', 3)
+    @submarine = Ship.new('submarine', 2)
   end
 end
