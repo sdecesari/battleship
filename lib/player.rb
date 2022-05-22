cc62d68a7c6d667701658c6e19dc03c110f4bf19
Shirley DeCesari <shirley.decesari@gmail.com>
2022-05-22 15:09:02 -0700

    Add shots fired method

--- lib/player.rb
+++ lib/player.rb
@@ -4,7 +4,7 @@ class Player
                 :cruiser,
                 :submarine
   def initialize(board)
-    @board = board
+    @player_board = board
     @cruiser = Ship.new('cruiser', 3)
     @submarine = Ship.new('submarine', 2)
   end
@@ -13,11 +13,11 @@ class Player
     p "I have laid out my ships on the grid."
     p "You now need to lay out your two ships."
     p "The cruiser is three units long and the submarine is two units long"
-    p @board.render
+    p @player_board.render
     p "Enter the squares for the cruiser (3 spaces):"
     cruiser_coordinates = gets.chomp.upcase.split(" ")
 
-    until @board.valid_placement?(@cruiser, cruiser_coordinates) == true do
+    until @player_board.valid_placement?(@cruiser, cruiser_coordinates) == true do
       p "Invalid coordinates. Please try again!"
       cruiser_coordinates = gets.chomp.upcase.split(" ")
     end
@@ -26,10 +26,20 @@ class Player
     p "Enter the squares for the submarine (2 spaces):"
     submarine_coordinates = gets.chomp.upcase.split(" ")
 
-    until @board.valid_placement?(@submarine, submarine_coordinates) == true do
+    until @player_board.valid_placement?(@submarine, submarine_coordinates) == true do
       p "Invalid coordinates. Please try again!"
       submarine_coordinates = gets.chomp.upcase.split(" ")
     end
-    @board.place(@submarine, submarine_coordinates)
+    @player_board.place(@submarine, submarine_coordinates)
+  end
+
+  def shots_fired
+    p "Enter the coordinate for your shot:"
+    player_coordinates = gets.chomp.capitalize
+
+    until @player_board.valid_coordinate?(player_coordinates) == true do
+      p "Please enter a valid coordinate:"
+      player_coordinates = gets.chomp.capitalize
+    end
   end
 end
